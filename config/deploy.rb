35f872ccb3fcb16de40a8712df2fdfd63a469eaf
hungmi <hungmihungmi@gmail.com>
2016-06-04 08:03:34 +0800

    comment out gem 'web_console'

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.4.0'
 
 set :application, 'jpking'
-set :repo_url, 'git@github.com:hungmihungmi/jpking.git' # Edit this to match your repository
+set :repo_url, 'git@github.com:hungmi/jpking.git' # Edit this to match your repository
 set :branch, :master
 set :deploy_to, '/home/deploy/jpking'
 set :pty, true
