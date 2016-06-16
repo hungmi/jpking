899d1ce5534323670f2437a4c05d80a6325515b6
hungmi <hungmihungmi@gmail.com>
2016-06-16 18:46:21 +0800

    add require 'headless'

--- app/services/fb_bot.rb
+++ app/services/fb_bot.rb
@@ -1,6 +1,7 @@
 class FbBot
   require 'capybara'
   require 'capybara/dsl'
+  require 'headless'
   require 'capybara-webkit'
   include Capybara::DSL
   Capybara.run_server = false
@@ -8,43 +9,47 @@ class FbBot
   Capybara.app_host = "https://www.facebook.com/"
 
   def initialize#(user,pass)
-    puts "登入中"
-    visit('/')
-    if page.has_selector?('input[name="email"]')
-      fill_in("email", with: "gn01189424@gmail.com")
-      fill_in("pass", with: "peter012")
-      find("input#u_0_m").click
-      # save_screenshot
-    else
-      puts "已登入"
+    Headless.ly do
+      puts "登入中"
+      visit('/')
+      if page.has_selector?('input[name="email"]')
+        fill_in("email", with: "gn01189424@gmail.com")
+        fill_in("pass", with: "peter012")
+        find("input#u_0_m").click
+        # save_screenshot
+      else
+        puts "已登入"
+      end
     end
     # save_and_open_screenshot
   end
 
   def get_single_post_orders(url)
-    visit("/#{url.gsub(Capybara.app_host,'')}")
-    @html = Nokogiri::HTML.parse(page.html)
-    i = 0
-    @results = {}
-    @orders = {}
-    # 頁面：該則貼文的專屬連結
-    # 例如：https://www.facebook.com/groups/880774278680822/permalink/1032736396817942/
-    # 整則貼文的文字
-    @product = find(".userContent").text
-    @picture = @html.search(".mtm img").attr('src').text
-    # 各回覆的專屬連結
-    @html.search(".UFIComment.UFIRow[aria-label='留言'] .UFICommentContent").each do |a|
-      customer = a.search(".UFICommentActorName").text
-      unless customer == "江佳蓉" # 若回覆有編輯過，會出現一個連結的title是 '顯示編輯紀錄'，要忽略掉
-        puts "正在抓取 comment##{i} ..."
-        # save_screenshot
-        @orders[customer] = a.search(".UFICommentBody").inner_html
-        @results[0] = { picture: @picture, title: @product, orders: @orders }
-        i = i + 1
+    Headless.ly do
+      visit("/#{url.gsub(Capybara.app_host,'')}")
+      @html = Nokogiri::HTML.parse(page.html)
+      i = 0
+      @results = {}
+      @orders = {}
+      # 頁面：該則貼文的專屬連結
+      # 例如：https://www.facebook.com/groups/880774278680822/permalink/1032736396817942/
+      # 整則貼文的文字
+      @product = find(".userContent").text
+      @picture = @html.search(".mtm img").attr('src').text
+      # 各回覆的專屬連結
+      @html.search(".UFIComment.UFIRow[aria-label='留言'] .UFICommentContent").each do |a|
+        customer = a.search(".UFICommentActorName").text
+        unless customer == "江佳蓉" # 若回覆有編輯過，會出現一個連結的title是 '顯示編輯紀錄'，要忽略掉
+          puts "正在抓取 comment##{i} ..."
+          # save_screenshot
+          @orders[customer] = a.search(".UFICommentBody").inner_html
+          @results[0] = { picture: @picture, title: @product, orders: @orders }
+          i = i + 1
+        end
       end
+      # binding.pry
+      return @results
     end
-    # binding.pry
-    return @results
   end
 
   def get_orders_in_post_comments(url)
